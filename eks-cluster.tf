9a12b39d9bf499ede7737edbf2abeb39b289accc
[freddie-entity] <nguyentrungtinc4nh1516@gmail.com>
2022-06-08 16:41:29 +0700

    Added grafana, alertmanager, alerts, metrics-server

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -14,17 +14,17 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.small"
+      instance_type                 = "t2.medium"
       additional_userdata           = "echo booting"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 3
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
+      instance_type                 = "t2.large"
       additional_userdata           = "echo booting"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 3
+      asg_desired_capacity          = 2
     },
   ]
 }
